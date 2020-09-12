00a024b270e175924f82170ab5a729674d4422a0
adityasrivastava <adityasrivastava010@gmail.com>
2020-09-13 00:37:09 +0530

    first commit

---
+++ src/main/java/com/sample/bidsystem/BidApplication.java
@@ -0,0 +1,12 @@
+package com.sample.bidsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidApplication.class, args);
+	}
+}
